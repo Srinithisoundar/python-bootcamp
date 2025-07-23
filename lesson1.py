b29540fd395b2f88468c80ebf8364fce65471687
Srinithisoundar <srinithisoundarapandian@gmail.com>
2025-07-23 20:49:19 +0530

    Add files via upload
    This is the first program executing simple print statement and variable and f string and multiline outputs

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+name = "Sri"
+age = 20
+print("My name is",name,"and my age is",age)
+print(f"my name is {name} and my age is {age}")
+print("my name is",name,'\nMy age is',age)
+print("""my name is,name,
+my age is ,age""")
+print(f"""Name:{name}
+Age:{age}""")
+print("Name:",name,"\nAge:",age)
